cadf93562939a6c88baf5e18da76bfd6c0c7edfb
qwerty7415963 <34638478+qwerty7415963@users.noreply.github.com>
2022-08-25 21:42:44 +0700

    Initial commit

---
+++ src/components/navigations/RootStack.tsx
@@ -0,0 +1,47 @@
+import {
+  StackNavigationProp,
+  createStackNavigator,
+} from '@react-navigation/stack';
+
+import Intro from '../pages/Intro';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import Temp from '../pages/Temp';
+import {useTheme} from 'dooboo-ui';
+
+export type RootStackParamList = {
+  default: undefined;
+  Intro: undefined;
+  Temp: {param: string};
+};
+
+export type RootStackNavigationProps<
+  T extends keyof RootStackParamList = 'default',
+> = StackNavigationProp<RootStackParamList, T>;
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+function RootStack(): React.ReactElement {
+  const {theme, themeType} = useTheme();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Intro"
+        screenOptions={{
+          headerMode: themeType === 'dark' ? 'screen' : 'float',
+          headerStyle: {
+            backgroundColor: theme.background,
+          },
+          headerTitleStyle: {color: theme.text},
+          headerTintColor: theme.primary,
+        }}
+      >
+        <Stack.Screen name="Intro" component={Intro} />
+        <Stack.Screen name="Temp" component={Temp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default RootStack;
